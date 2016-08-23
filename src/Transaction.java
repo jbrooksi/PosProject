5b7d140e2eb0efcd76861aafb6fa523c597dc51f
Heather Beaton <hamcdonough78@yahoo.com>
2016-08-23 14:59:42 -0400

    added transaction class

---
+++ src/Transaction.java
@@ -0,0 +1,30 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+
+public class Transaction {
+	
+	private Product item;
+	private int quatity;
+	
+	//private ArrayList<Product> itemPurchased = new ArrayList<Product>();
+	
+	public Transaction(Product item, int quatity) {
+		super();
+		this.item = item;
+		this.quatity = quatity;
+	}
+	
+	public double subTotal () {
+		
+		
+		double tax = 0.06;
+		
+		double pr = item.getPrice();
+		double subTotal =  quatity * (pr + tax * pr);
+		subTotal = new BigDecimal(subTotal)
+				.setScale(2, RoundingMode.HALF_UP).doubleValue();
+		return subTotal;
+	} 
+	
+}	
